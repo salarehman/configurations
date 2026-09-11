660cb11df0333fbdd447e40aecf12a93ec0813d3
Lily-Jiji <salahdin.ur-rehman@proton.me>
2026-09-11 20:44:29 +0200

    snapshot: laptop_2026-09-11_20-44-28

---
+++ snapshots/laptop_2026-09-11_20-44-28/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "my first flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-doom-emacs-unstraightened = {
+      url = "github:marienz/nix-doom-emacs-unstraightened";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      laptop = lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };   # <- makes `inputs` available inside configuration.nix
+        modules = [
+          ./configuration.nix
+          ./config-backup.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };   # <- makes `inputs` available inside your home-manager user config
+          }
+	  ./git-config.nix
+        ];
+      };
+    };
+  };
+}
